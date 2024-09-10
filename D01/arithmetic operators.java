7a545e0ee096ebae881657e1ab44f85ebd188e43
GIRIDHAR SK <151628450+Giridhar0702@users.noreply.github.com>
2024-09-10 21:25:33 +0530

    Create arithmetic operators.java

---
+++ D01/arithmetic operators.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[]args)
+    {
+        int num=10;
+        int num1=5;
+        System.out.println(num+num1);
+        System.out.println(num-num1);
+        System.out.println(num*num1);
+        System.out.println(num/num1);
+        
+        
+    }
+}
