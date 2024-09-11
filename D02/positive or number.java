eca81794428967ee33fddfeefa9a8042f54c3669
GIRIDHAR SK <151628450+Giridhar0702@users.noreply.github.com>
2024-09-11 22:21:36 +0530

    Create positive or number.java

---
+++ D02/positive or number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+  public static void main(String[]args){
+    Scanner input=new Scanner(System.in);
+    int num=input.nextInt();
+    if(num>0)
+    {
+      System.out.print(num+" is Positive number");
+   }
+    else{
+      System.out.print(num +" is Negative number");
+}
+}
+}
