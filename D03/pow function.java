ff18a5c09ceff0704fa9d209cf8e380b33cb680d
GIRIDHAR SK <151628450+Giridhar0702@users.noreply.github.com>
2024-09-12 21:47:44 +0530

    Create pow function.java

---
+++ D03/pow function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        int ans;
+        ans=(int)(Math.pow(n, m));
+        System.out.print(ans);
+        
+    }
+}
